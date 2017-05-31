59580b971d4fe66efe60d88af894d79fe1a26880
George Raptis <georapbox@gmail.com>
2017-05-31 14:35:52 +0300

    use eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,12 @@ module.exports = {
         test: /(\.jsx|\.js)$/,
         loader: 'babel-loader',
         exclude: /(node_modules|bower_components)/
-      }
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+}
     ]
   },
   devServer: {
